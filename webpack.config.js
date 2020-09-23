bcbb167f4eb53d8b694542f966e99610f3fc7dea
Illia Zolotarov <izolotarov@s-pro.io>
2020-09-23 13:52:21 +0300

    Finished environment settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,81 @@
-const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
+const isProd = process.env.NODE_ENV === "production";
+const isDev = !isProd;
+
+const filename = (ext) => (isDev ? `bandle.${ext}` : `bandle.[hash].${ext}`);
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    mode : 'development',
-    entry: './index.js',
-    output: {
-        filename: 'bandle.[hash].js',
-        path: path.resolve(__dirname, 'dist')
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: ["@babel/polyfill", "./index.js"],
+  output: {
+    filename: filename("js"),
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".js"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@core": path.resolve(__dirname, "src/core"),
     },
-    resolve: {
-        extensions: ['.js'],
-        alias: {
-            '@': path.resolve(__dirname, 'src')
-            '@core': path.resolve(__dirname, 'src/core')
-        }
-    }
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: 'index.html'
-        }),
-        new CopyPlugin({
+  },
+  devtool: isDev && "source-map",
+  devServer: {
+    port: 3000,
+    hot: isDev,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "index.html",
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CopyPlugin({
       patterns: [
-        { from: path.resolve(__dirname, 'src/favicon.ico'), to: path.resolve(__dirname, 'dist') }
-      ]
+        {
+          from: path.resolve(__dirname, "src/favicon.ico"),
+          to: path.resolve(__dirname, "dist"),
+        },
+      ],
     }),
     new MiniCssExtractPlugin({
-          filename: 'bandle.[hash].js',
-    })
-    ],
-     rules: [
+      filename: filename("css"),
+    }),
+  ],
+  module: {
+    rules: [
       {
         test: /\.s[ac]ss$/i,
         use: [
-        MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true,
+            },
+          },
+          "css-loader",
+          "sass-loader",
         ],
       },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
     ],
-}
+  },
+};
