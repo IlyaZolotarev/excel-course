c69a4a56928c60f780b9fb59d58da729194bd752
izolotarov <izolotarov@s-pro.io>
2020-09-23 12:16:55 +0300

    added new webpack settings

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode : 'development',
+    entry: './index.js',
+    output: {
+        filename: 'bandle.[hash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+            '@core': path.resolve(__dirname, 'src/core')
+        }
+    }
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'src/favicon.ico'), to: path.resolve(__dirname, 'dist') }
+      ]
+    }),
+    new MiniCssExtractPlugin({
+          filename: 'bandle.[hash].js',
+    })
+    ],
+     rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+        MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+}
